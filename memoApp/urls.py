19444410ca2e34ee8b991d11700eff602a725171
gustavo <gustavonerybarbosa@gmail.com>
2023-04-13 17:48:21 -0300

    1.3 fix file_detail and views

--- memoApp/urls.py
+++ memoApp/urls.py
@@ -2,8 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('upload/', views.upload, name='upload'),
+    path('', views.upload, name='upload'),
     path('upload_success/', views.file_list, name='file_list'),
-    path('file_detail<int:pk>.html', views.file_detail, name='file_detail'),
-    path('upload.html', views.upload, name='upload-html'),
+    path('file_detail/<id>', views.file_detail, name='file_detail'),
 ]
--- memoApp/views.py
+++ memoApp/views.py
@@ -5,28 +5,57 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django import forms
 from .models import Image
+from django.http import Http404
+from .forms import ImageForm
 
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = ('name', 'file')
-
-    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
-
+@login_required
 def upload(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            files = Image.objects.all()
-            return render(request, 'upload_success.html', {'files': files})
+            image=form.save()
+            context={
+                'image': image
+            }
+            return render(request, 'upload_success.html', context)
     else:
         form = ImageForm()
-    return render(request, 'upload.html', {'form': form})
 
-def file_detail(request, pk):
-    image = get_object_or_404(Image, pk=pk)
-    return render(request, 'file_detail.html', {'image': image})
+    context={
+        'form': form
+        }
+    return render(request, 'upload.html', context)
+
+@login_required
+def file_detail(request, id):
+    try:
+        image = Image.objects.get(id=id)
+    except Image.DoesNotExist:
+        raise Http404("Image does not exist")
+
+    context = {'image': image}
+
+    return render(request, 'file_detail.html', context)
+
+# @login_required
+# def emAtendimento(request, id):
+#     try:
+#         atendimento = Atendimento.objects.get(id=id)
+#         atendimento.emAtendimento()
+#         context={
+#         'senha': atendimento,
+#         }        
+#     except:
+#         context={
+#         'senha': '',
+#         }
+#     return render(request, 'em-atendimento.html', context)
+
+
+# def file_detail(request, pk):
+#     image = get_object_or_404(Image, pk=pk)
+#     return render(request, 'file_detail.html', {'image': image})
+
 
 def file_list(request):
     files = Image.objects.all()
