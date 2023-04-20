6d6326775449fa2987fbe37e33a22305d1281b50
gustavo <gustavonerybarbosa@gmail.com>
2023-04-20 17:41:07 -0300

    2.0 drag and drop (inputs of archives) =)

---
+++ memoApp/static/js/memoMain.js
@@ -0,0 +1,30 @@
+const fileInput = document.querySelector('#id_file')
+const dropArea = document.getElementById('drop-area');
+
+dropArea.addEventListener('dragover', handleDragOver);
+dropArea.addEventListener('drop', handleFileDrop);
+
+function handleDragOver(event) {
+    event.preventDefault();
+    dropArea.classList.add('drag-over');
+}
+
+function handleFileDrop(event) {
+    event.preventDefault();
+    dropArea.classList.remove('drag-over');
+    const files = event.dataTransfer.files;
+    fileInput.files = files;
+}
+
+const inputContainer = document.getElementById('input-container');
+const addInputButton = document.getElementById('addInput');
+
+addInputButton.addEventListener('click', () => {
+    const newInput = document.createElement('input');
+    newInput.type = 'file';
+    newInput.name = 'file';
+    newInput.id = 'id_file'; 
+    inputContainer.appendChild(document.createElement('br'));
+    inputContainer.appendChild(newInput);
+
+});
