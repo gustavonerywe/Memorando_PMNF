be22dbb3882928f5d1d7079012033c5428adec61
gustavo <gustavonerybarbosa@gmail.com>
2023-05-23 16:38:56 -0300

    5.7 cancel-button in inputs archives

--- memoApp/static/js/memoMain.js
+++ memoApp/static/js/memoMain.js
@@ -16,38 +16,86 @@ function handleFileDrop(event) {
     fileInput.files = files;
 }
 
-const inputContainer = document.getElementById('input-container');
+// const inputContainer = document.getElementById('input-container');
+// const addInputButton = document.getElementById('addInput');
+
+// addInputButton.addEventListener('click', () => {
+//     const newInput = document.createElement('input');
+//     newInput.type = 'file';
+//     newInput.name = 'file';
+//     newInput.id = 'id_file';
+//     inputContainer.appendChild(document.createElement('br'));
+//     inputContainer.appendChild(newInput);
+
+// });
+
+const inputContainer = document.getElementById('container-file');
 const addInputButton = document.getElementById('addInput');
 
-addInputButton.addEventListener('click', () => {
-    const newInput = document.createElement('input');
-    newInput.type = 'file';
-    newInput.name = 'file';
-    newInput.id = 'id_file';
-    inputContainer.appendChild(document.createElement('br'));
-    inputContainer.appendChild(newInput);
+function createNewInput() {
+  const inputRow = document.createElement('div');
+  inputRow.classList.add('input-row');
 
+  const cancelButton = document.createElement('button');
+  cancelButton.classList.add('cancel-button');
+  cancelButton.textContent = 'x';
+
+  const fileInput = document.createElement('input');
+  fileInput.type = 'file';
+  fileInput.name = 'file';
+  fileInput.classList.add('file-input');
+  fileInput.multiple = true;
+  fileInput.required = true;
+
+  inputRow.appendChild(cancelButton);
+  inputRow.appendChild(fileInput);
+  inputContainer.insertBefore(inputRow, addInputButton.parentNode);
+
+  cancelButton.addEventListener('click', function() {
+    let conf = confirm('Deseja cancelar o arquivo?');
+    if (conf) {
+      fileInput.value = '';
+    }
+  });
+}
+
+function addCancelButtonEvent(cancelButton) {
+  cancelButton.addEventListener('click', function() {
+    let conf = confirm('Deseja cancelar o arquivo?');
+    if (conf) {
+      const fileInput = cancelButton.nextElementSibling;
+      fileInput.value = '';
+    }
+  });
+}
+
+addInputButton.addEventListener('click', function() {
+  createNewInput();
+  const cancelButtons = document.querySelectorAll('.cancel-button');
+  cancelButtons.forEach(function(cancelButton) {
+    addCancelButtonEvent(cancelButton);
+  });
 });
 
+const firstCancelButton = document.querySelector('.cancel-button');
+addCancelButtonEvent(firstCancelButton);
+
+
+
 function pdfReader(dados) {
-    // Seu código JavaScript
     const pdfUrl = dados;
     const canvas = document.getElementById('pdf-canvas');
     const ctx = canvas.getContext('2d');
 
-    // Carrega o arquivo PDF
     pdfjsLib.getDocument(pdfUrl).promise
         .then((pdf) => {
-            // Carrega a primeira página do PDF
             return pdf.getPage(1);
         })
         .then((page) => {
-            // Calcula as dimensões do canvas para caber a página inteira do PDF
             const viewport = page.getViewport({ scale: 1 });
             canvas.width = viewport.width;
             canvas.height = viewport.height;
-
-            // Renderiza a página do PDF no canvas
+            
             const renderContext = {
                 canvasContext: ctx,
                 viewport: viewport,
@@ -70,11 +118,22 @@ fetch('/digital_view/')
 
 const btn = document.querySelector('#botao')
 
-botao.addEventListener('click', function(event){
+botao.addEventListener('click', function (event) {
     event.preventDefault()
 
     let confirmed = confirm('Deseja mesmo enviar o formulário?')
-    if(confirmed){
+    if (confirmed) {
         event.target.closest('form').submit()
-    } 
+    }
 })
+
+// let cancelElement = document.querySelector('#cancel-button')
+
+// fileInput.parentNode.insertBefore(cancelElement, fileInput.nextSibling)
+
+// cancelElement.addEventListener('click', function(){
+//     let conf = confirm('Deseja cancelar o arquivo?')
+//     if(conf){
+//         fileInput.value = ''
+//     }
+// })
