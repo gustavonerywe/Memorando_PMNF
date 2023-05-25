5a96ead949754922ce6711951271c5062c2b7a52
gustavo <gustavonerybarbosa@gmail.com>
2023-05-25 16:51:14 -0300

    6.1 show-modal  if(cancel-button)

--- memoApp/static/js/memoMain.js
+++ memoApp/static/js/memoMain.js
@@ -5,15 +5,15 @@ dropArea.addEventListener('dragover', handleDragOver);
 dropArea.addEventListener('drop', handleFileDrop);
 
 function handleDragOver(event) {
-    event.preventDefault();
-    dropArea.classList.add('drag-over');
+  event.preventDefault();
+  dropArea.classList.add('drag-over');
 }
 
 function handleFileDrop(event) {
-    event.preventDefault();
-    dropArea.classList.remove('drag-over');
-    const files = event.dataTransfer.files;
-    fileInput.files = files;
+  event.preventDefault();
+  dropArea.classList.remove('drag-over');
+  const files = event.dataTransfer.files;
+  fileInput.files = files;
 }
 
 // const inputContainer = document.getElementById('input-container');
@@ -31,6 +31,8 @@ function handleFileDrop(event) {
 
 const inputContainer = document.getElementById('container-file');
 const addInputButton = document.getElementById('addInput');
+var meuModal = document.getElementById("meuModal");
+var fechar = document.querySelector(".fechar");
 
 function createNewInput() {
   const inputRow = document.createElement('div');
@@ -38,6 +40,7 @@ function createNewInput() {
 
   const cancelButton = document.createElement('button');
   cancelButton.classList.add('cancel-button');
+  cancelButton.type = 'button';
   cancelButton.textContent = 'x';
 
   const fileInput = document.createElement('input');
@@ -51,28 +54,44 @@ function createNewInput() {
   inputRow.appendChild(fileInput);
   inputContainer.insertBefore(inputRow, addInputButton.parentNode);
 
-  cancelButton.addEventListener('click', function() {
-    let conf = confirm('Deseja cancelar o arquivo?');
-    if (conf) {
-      fileInput.value = '';
+  cancelButton.addEventListener('click', function () {
+    meuModal.style.display = "block";
+    console.log('oi')
+  });
+
+  fechar.addEventListener("click", function () {
+    meuModal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target == meuModal) {
+      meuModal.style.display = "none";
     }
   });
 }
 
 function addCancelButtonEvent(cancelButton) {
-  cancelButton.addEventListener('click', function() {
-    let conf = confirm('Deseja cancelar o arquivo?');
-    if (conf) {
-      const fileInput = cancelButton.nextElementSibling;
-      fileInput.value = '';
+  cancelButton.type = 'button';
+  cancelButton.addEventListener('click', function () {
+    meuModal.style.display = "block";
+    console.log('oi')
+  });
+
+  fechar.addEventListener("click", function () {
+    meuModal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target == meuModal) {
+      meuModal.style.display = "none";
     }
   });
 }
 
-addInputButton.addEventListener('click', function() {
+addInputButton.addEventListener('click', function () {
   createNewInput();
   const cancelButtons = document.querySelectorAll('.cancel-button');
-  cancelButtons.forEach(function(cancelButton) {
+  cancelButtons.forEach(function (cancelButton) {
     addCancelButtonEvent(cancelButton);
   });
 });
@@ -81,59 +100,65 @@ const firstCancelButton = document.querySelector('.cancel-button');
 addCancelButtonEvent(firstCancelButton);
 
 
-
 function pdfReader(dados) {
-    const pdfUrl = dados;
-    const canvas = document.getElementById('pdf-canvas');
-    const ctx = canvas.getContext('2d');
+  const pdfUrl = dados;
+  const canvas = document.getElementById('pdf-canvas');
+  const ctx = canvas.getContext('2d');
 
-    pdfjsLib.getDocument(pdfUrl).promise
-        .then((pdf) => {
-            return pdf.getPage(1);
-        })
-        .then((page) => {
-            const viewport = page.getViewport({ scale: 1 });
-            canvas.width = viewport.width;
-            canvas.height = viewport.height;
-            
-            const renderContext = {
-                canvasContext: ctx,
-                viewport: viewport,
-            };
-            return page.render(renderContext);
-        })
-        .catch((error) => {
-            console.error('Erro ao carregar o arquivo PDF:', error);
-        });
+  pdfjsLib.getDocument(pdfUrl).promise
+    .then((pdf) => {
+      return pdf.getPage(1);
+    })
+    .then((page) => {
+      const viewport = page.getViewport({ scale: 1 });
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
+
+      const renderContext = {
+        canvasContext: ctx,
+        viewport: viewport,
+      };
+      return page.render(renderContext);
+    })
+    .catch((error) => {
+      console.error('Erro ao carregar o arquivo PDF:', error);
+    });
 
 }
 
 fetch('/digital_view/')
-    .then((response) => {
-        return response.json()
-    })
-    .then((dados) => {
-        pdfReader(dados)
-    })
+  .then((response) => {
+    return response.json()
+  })
+  .then((dados) => {
+    pdfReader(dados)
+  })
 
 const btn = document.querySelector('#botao')
 
 botao.addEventListener('click', function (event) {
-    event.preventDefault()
+  event.preventDefault()
 
-    let confirmed = confirm('Deseja mesmo enviar o formulário?')
-    if (confirmed) {
-        event.target.closest('form').submit()
-    }
+  let confirmed = confirm('Deseja mesmo enviar o formulário?')
+  if (confirmed) {
+    event.target.closest('form').submit()
+  }
 })
 
-// let cancelElement = document.querySelector('#cancel-button')
 
-// fileInput.parentNode.insertBefore(cancelElement, fileInput.nextSibling)
+const buttonYesModal = document.querySelector('#buttonYesModal')
+const buttonNoModal = document.querySelector('#buttonNoModal')
+const spanMessage = document.querySelector('#spanMessage')
 
-// cancelElement.addEventListener('click', function(){
-//     let conf = confirm('Deseja cancelar o arquivo?')
-//     if(conf){
-//         fileInput.value = ''
-//     }
-// })
+buttonYesModal.addEventListener('click', function () {
+  document.querySelector('.file-input').value = '';
+  meuModal.style.display = "none";
+  spanMessage.textContent = "Arquivo removido com sucesso!"
+  setTimeout(function(){
+    spanMessage.textContent = ''
+  }, 2500)
+})
+
+buttonNoModal.addEventListener('click', function () {
+  meuModal.style.display = "none";
+})
