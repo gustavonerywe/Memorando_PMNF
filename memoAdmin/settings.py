660156b097dc29740ad5d577624ff971797044ec
YanIgnacio <yandasilva3@gmail.com>
2023-08-10 11:09:18 -0300

    19.3 database mysql working

--- memoAdmin/settings.py
+++ memoAdmin/settings.py
@@ -82,10 +82,22 @@ WSGI_APPLICATION = 'memoAdmin.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'memorando',
+        'PORT': '3306',
+
+        'USER': 'memouser',
+        'PASSWORD': 'memo@pmnf2023',
+        'HOST': '192.168.3.210',
+    },
+    
 }
 
 
