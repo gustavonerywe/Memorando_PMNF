17bce47a4fd7fd416a2bc71759fe5b5e00a8fc73
gustavo <gustavonerybarbosa@gmail.com>
2023-08-26 13:11:58 -0300

    3.3.3 numBusca as important

--- memoApp/static/js/consulta.js
+++ memoApp/static/js/consulta.js
@@ -11,3 +11,38 @@ backPage.addEventListener('click', () =>{
     window.history.back()
 })
 
+document.addEventListener("DOMContentLoaded", function () {
+    let inputNumero = document.getElementById("numBusca");
+    let clearButton = document.getElementById("clearButton");
+    let storedNumero = localStorage.getItem("numBusca");
+    if (storedNumero) {
+        inputNumero.value = storedNumero;
+    }
+
+    clearButton.addEventListener("click", function () {
+        inputNumero.value = "";
+    });
+});
+
+document.querySelector("form").addEventListener("submit", function (event) {
+    let inputNumero = document.getElementById("numBusca");
+    localStorage.setItem("numBusca", inputNumero.value);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    let termoBusca = document.getElementById("termoBusca");
+    let removeBusca = document.getElementById("removeBusca");
+    let storedTermo = localStorage.getItem("termoBusca");
+    if (storedTermo) {
+        termoBusca.value = storedTermo;
+    }
+
+    removeBusca.addEventListener("click", function () {
+        termoBusca.value = "";
+    });
+});
+
+document.querySelector("form").addEventListener("submit", function (event) {
+    let termoBusca = document.getElementById("termoBusca");
+    localStorage.setItem("termoBusca", termoBusca.value);
+});
