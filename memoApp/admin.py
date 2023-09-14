c7062789476e5f0ad3553dc5289ded25a61906d9
gustavo <>
2023-09-14 15:49:35 -0300

    3.6.1 adding the address in admin

--- memoApp/admin.py
+++ memoApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.models import User
 
 
@@ -13,6 +14,17 @@ class UserInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = [UserInline]
 
+class GroupInline(admin.StackedInline):
+    model = GroupMoc
+    can_delete = False
+    verbose_name_plural = "groupmoc"
+
+class GroupAdmin(BaseGroupAdmin):
+    inlines = [GroupInline]
+
+
+admin.site.unregister(Group)
+admin.site.register(Group, GroupAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
